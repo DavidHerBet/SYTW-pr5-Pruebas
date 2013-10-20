4f465fee934fcb0bb9246de286d72248cdb65993
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-10-20 19:44:02 +0100

    Cambiar al config.ru la info del servidor

--- lib/rps.rb
+++ lib/rps.rb
@@ -2,11 +2,6 @@ require 'rack/request'
 require 'rack/response'
 require 'haml'
 
-# Indica en que puerto y con que servidor se ejecuta la aplicación
-puts "> Iniciando servidor Thin"
-puts "> Aplicación RPS"
-puts "> http://localhost:9292/"
-  
 module RockPaperScissors
   class App 
  
