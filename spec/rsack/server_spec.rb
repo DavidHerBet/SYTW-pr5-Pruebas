0bd35289f5592fd447b09413c4382e3d0e977753
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-10-23 21:51:39 +0100

    Añadir pruebas para cada elección

--- spec/rsack/server_spec.rb
+++ spec/rsack/server_spec.rb
@@ -2,15 +2,46 @@ require 'spec_helper'
 
 describe Rsack::Server do
 
-  #let(:server) { Rack::MockRequest.new(Rsack::Server.new) }
   def server
-    Rack::MockRequest.new(Rsack::Server.new) 
+    Rack::MockRequest.new(Rsack::Server.new(RockPaperScissors::App.new))
   end
 
   context '/' do
-    it "should return a 200 code" do
+    it "Should return a 200 code" do
       response = server.get('/')
       response.status.should == 200
     end
+  end
+
+  context '/?choice=rock' do
+    it "Should return a 200 code" do
+      response = server.get('/')
+      response.status.should == 200
+    end
+  end
+
+  context '/?choice=paper' do
+    it "Should return a 200 code" do
+      response = server.get('/')
+      response.status.should == 200
+    end
+  end
+
+  context '/?choice=scissors' do
+    it "Should return a 200 code" do
+      response = server.get('/')
+      response.status.should == 200
+    end
+  end
+
+  context '/?choice=reset' do
+    it "Should return a 200 code" do
+      response = server.get('/')
+      response.status.should == 200
+    end
+  end
+
+  it "Should exist a player throw" do
+    server.respond_to?("player_throw").should == true
   end
 end
